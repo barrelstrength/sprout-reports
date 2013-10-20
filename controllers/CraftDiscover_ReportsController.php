b1a1e2dea56d8c28720cd47f15d0862be92d69b2
Adrienne Travis <adrienne@utilitarienne.com>
2013-10-20 08:10:49 -0500

    export as Csv

--- controllers/CraftDiscover_ReportsController.php
+++ controllers/CraftDiscover_ReportsController.php
@@ -1,26 +1,32 @@
 <?php
 namespace Craft;
-use Paste as Paste;
+use Paste;
+use SimpleExcel\SimpleExcel;
+use SimpleExcel\Spreadsheet\Worksheet;
 
 class CraftDiscover_ReportsController extends BaseController
 {
     public function actionCreateQuery()
     {
         $this->requirePostRequest();
+        $exportData = craft()->request->getPost('exportData');
         $queryText = craft()->request->getPost('queryText');
         $elementType = craft()->request->getPost('elementType');
         $sectionName = craft()->request->getPost('sectionName');
         $isUsersQuery = craft()->request->getPost('isUsersQuery');
         $tableCols = array();
+        $qType = 'Query';
 
         if($queryText) {
             $myentries = craft()->db->createCommand($queryText)->queryAll();
             if(sizeof($myentries) > 0) {
                 $tableCols = array_keys($myentries[0]);
                 }
+
             }
 
         elseif($isUsersQuery) {
+            $qType = 'Users';
             $myentries = craft()->db->createCommand()
                 ->select('photo, id, username, firstName, lastName, email, admin AS isAdmin')
                 ->from('users u')
@@ -37,6 +43,7 @@ class CraftDiscover_ReportsController extends BaseController
 
         elseif($sectionName) {
             $criteria = craft()->sections->getSectionById($sectionName);
+            $qType = 'Elements'.$criteria->name;
             $sectionid = $criteria->id;
 
             $combination = craft()->request->getPost('combination');
@@ -66,8 +73,26 @@ class CraftDiscover_ReportsController extends BaseController
                 }
             }
 
+    if($exportData == 'Y') {
+        $worksheet = new Worksheet();
 
-     return craft()->urlManager->setRouteVariables(array('queryText' =>$queryText, 'sectionName' => $sectionName, 'myEntries' => $myentries, 'tableCols' => $tableCols));
+        foreach($myentries as $key=>$row) {
+            unset($row['photo']);
+            if($key == 0) {
+                $mycolnames = array_keys($row);
+                $worksheet->insertRecord($mycolnames);
+                }
+            $worksheet->insertRecord($row);
+            }
+
+        $excel = new SimpleExcel();
+        $excel->insertWorksheet($worksheet);
+
+        $filename = 'export'.$qType.time().'.csv';
+        $excel->exportFile(CRAFT_STORAGE_PATH.$filename, 'CSV');
+        $excel->exportFile('php://output', 'CSV', array('filename' => $filename));
+        }
+    return craft()->urlManager->setRouteVariables(array('queryText' =>$queryText, 'sectionName' => $sectionName, 'myEntries' => $myentries, 'tableCols' => $tableCols));
 
   //      echo Paste\Pre::render( craft()->fields->getAllGroups() );
   //      echo Paste\Pre::render( craft()->fields->getAllFields() );

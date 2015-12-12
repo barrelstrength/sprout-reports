959a8ed6ebdadb2d723ab646a9796c64b9dec011
Ben Parizek <ben@barrelstrengthdesign.com>
2015-12-12 14:17:53 -0500

    Updates Category and Query reports with better naming and error messages
    - Moves logic from Category Report template into getOptionsHtml() method

--- integrations/sproutreports/datasources/SproutReportsCategoriesDataSource.php
+++ integrations/sproutreports/datasources/SproutReportsCategoriesDataSource.php
@@ -16,9 +16,9 @@ class SproutReportsCategoriesDataSource extends SproutReportsBaseDataSource
 	public function getDefaultLabels()
 	{
 		return array(
-				'Category Name',
-				'Total Entries assigned this category',
-				'% of total categories used'
+			'Category Name',
+			'Total Entries Assigned Category',
+			'% of Total Categories'
 		);
 	}
 
@@ -31,40 +31,39 @@ class SproutReportsCategoriesDataSource extends SproutReportsBaseDataSource
 	{
 		$options = $report->getOptions();
 
-		$sectionId = $options['sectionId'];
+		$sectionId       = $options['sectionId'];
 		$categoryGroupId = $options['categoryGroupId'];
 
-
-		$criteria = craft()->elements->getCriteria(ElementType::Category);
-		$criteria->limit = null;
+		$criteria          = craft()->elements->getCriteria(ElementType::Category);
+		$criteria->limit   = null;
 		$criteria->groupId = $categoryGroupId;
 
-		$categories = $criteria->find(array('indexBy'=>'id'));
+		$categories  = $criteria->find(array('indexBy' => 'id'));
 		$categoryIds = array_keys($categories);
 
-		$criteria = craft()->elements->getCriteria(ElementType::Entry);
+		$criteria            = craft()->elements->getCriteria(ElementType::Entry);
 		$criteria->sectionId = $sectionId;
-		$entryIds = $criteria->ids();
+		$entryIds            = $criteria->ids();
 
 		$totalCategories = craft()->db->createCommand()
-				->select('COUNT(*)')
-				->from('relations')
-				->where(array('in', "{{relations.sourceId}}", $entryIds))
-				->andWhere(array('in', "{{relations.targetId}}", $categoryIds))
-				->queryScalar();
+			->select('COUNT(*)')
+			->from('relations')
+			->where(array('in', "{{relations.sourceId}}", $entryIds))
+			->andWhere(array('in', "{{relations.targetId}}", $categoryIds))
+			->queryScalar();
 
 		$entries = craft()->db->createCommand()
-				->select("{{content.title}} AS 'Category Name',
-									COUNT({{relations.sourceId}}) AS 'Total Entries assigned this category',
-									(COUNT({{relations.sourceId}}) / $totalCategories) AS '% of total categories used'
-					")
-				->from('content')
-				->join('categories', '{{content.elementId}} = {{categories.id}}')
-				->join('relations', '{{relations.targetId}} = {{categories.id}}')
-				->where(array('in', "{{relations.sourceId}}", $entryIds))
-				->andWhere(array('in', "{{relations.targetId}}", $categoryIds))
-				->group('{{relations.targetId}}')
-				->queryAll();
+			->select("{{content.title}} AS 'Category Name',
+								COUNT({{relations.sourceId}}) AS 'ASDASD Entries assigned this category',
+								(COUNT({{relations.sourceId}}) / $totalCategories) AS '% of total categories used'
+				")
+			->from('content')
+			->join('categories', '{{content.elementId}} = {{categories.id}}')
+			->join('relations', '{{relations.targetId}} = {{categories.id}}')
+			->where(array('in', "{{relations.sourceId}}", $entryIds))
+			->andWhere(array('in', "{{relations.targetId}}", $categoryIds))
+			->group('{{relations.targetId}}')
+			->queryAll();
 
 		return $entries;
 	}
@@ -76,6 +75,43 @@ class SproutReportsCategoriesDataSource extends SproutReportsBaseDataSource
 	 */
 	public function getOptionsHtml(array $options = array())
 	{
-		return craft()->templates->render('sproutreports/datasources/_options/categories', compact('options'));
+		$sectionOptions       = array();
+		$categoryGroupOptions = array();
+		$errorMessage         = '';
+
+		$sections = craft()->sections->getAllSections();
+
+		foreach ($sections as $section)
+		{
+			if ($section->type != 'single')
+			{
+				$sectionOptions[] = array(
+					'label' => $section->name,
+					'value' => $section->id
+				);
+			}
+		}
+
+		$categoryGroups = craft()->categories->getAllGroups();
+
+		foreach ($categoryGroups as $categoryGroup)
+		{
+			$categoryGroupOptions[] = array(
+				'label' => $categoryGroup->name,
+				'value' => $categoryGroup->id
+			);
+		}
+
+		if (empty($sectionOptions) or empty($categoryGroupOptions))
+		{
+			$errorMessage = Craft::t('This report requires a Channel or Structure section using Categories. Please update your settings to include at least one Channel or Structure and at least one Category Group with Categories available to assign to that section.');
+		}
+
+		return craft()->templates->render('sproutreports/datasources/_options/categories', array(
+			'options'              => $options,
+			'sectionOptions'       => $sectionOptions,
+			'categoryGroupOptions' => $categoryGroupOptions,
+			'errorMessage'         => $errorMessage
+		));
 	}
 }

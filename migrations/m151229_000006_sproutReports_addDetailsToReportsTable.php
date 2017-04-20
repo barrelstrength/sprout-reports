7333125238f633e12e39c2dbc265d5086e4592cf
Dale Ramirez <dale@barrelstrengthdesign.com>
2017-04-20 13:43:42 +0800

    Fix error on migration when upgrading to latest version

--- migrations/m151229_000006_sproutReports_addDetailsToReportsTable.php
+++ migrations/m151229_000006_sproutReports_addDetailsToReportsTable.php
@@ -8,9 +8,9 @@ class m151229_000006_sproutReports_addDetailsToReportsTable extends BaseMigratio
 	 */
 	public function safeUp()
 	{
-		SproutReportsPlugin::log('Creating `dataSourceId` index.', LogLevel::Info);
+		//SproutReportsPlugin::log('Creating `dataSourceId` index.', LogLevel::Info);
 
-		craft()->db->createCommand()->createIndex('sproutreports_reports', 'dataSourceId');
+		//craft()->db->createCommand()->createIndex('sproutreports_reports', 'dataSourceId');
 
 		SproutReportsPlugin::log('Dropping groupId Foreign Key.', LogLevel::Info, true);
 

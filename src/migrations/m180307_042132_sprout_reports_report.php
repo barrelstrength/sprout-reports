adada09bfe94e72aceb385c906d8437024eb3cd4
Dale Ramirez <dale@barrelstrengthdesign.com>
2018-03-08 19:18:21 +0800

    - Included Users and Categories class when storing dataSources classes table only when separate plugin is activated - Created updateChangedDataSourceId migration that updates previous datasource id string from craft 2 to dataSourceId integer on dataSource table id

--- src/migrations/m180307_042132_sprout_reports_report.php
+++ src/migrations/m180307_042132_sprout_reports_report.php
@@ -6,6 +6,10 @@ use barrelstrength\sproutbase\SproutBase;
 use barrelstrength\sproutreports\integrations\sproutreports\datasources\CustomQuery;
 use barrelstrength\sproutreports\integrations\sproutreports\datasources\CustomTwigTemplate;
 use craft\db\Migration;
+use craft\db\Query;
+use barrelstrength\sproutreportscategories\integrations\sproutreports\datasources\Categories;
+use barrelstrength\sproutreportsusers\integrations\sproutreports\datasources\Users;
+use Craft;
 
 /**
  * m180307_042132_sprout_reports_report migration.
@@ -24,12 +28,51 @@ class m180307_042132_sprout_reports_report extends Migration
             CustomTwigTemplate::class
         ];
 
+        if (Craft::$app->getPlugins()->getPlugin('sprout-reports-users')) {
+            $dataSourceClasses[] = Users::class;
+        }
+
+        if (Craft::$app->getPlugins()->getPlugin('sprout-reports-categories')) {
+            $dataSourceClasses[] = Categories::class;
+        }
+
         SproutBase::$app->dataSources->installDataSources($dataSourceClasses);
 
         $this->dropColumn('{{%sproutreports_datasources}}', 'dataSourceId');
         $this->dropColumn('{{%sproutreports_datasources}}', 'options');
 
+        $this->addColumn('{{%sproutreports_reports}}', 'hasNameFormat', $this->integer()->after('name'));
+
+        $this->updateChangedDataSourceId();
+
         return true;
+    }
+
+    private function updateChangedDataSourceId()
+    {
+        $sources = [
+            'sproutreports.query'      => CustomQuery::class,
+            'sproutreports.users'      => Users::class,
+            'sproutreports.categories' => Categories::class
+        ];
+
+        if (Craft::$app->getPlugins()->getPlugin('sprout-reports-users')) {
+            $sources['sproutreports.users'] = Users::class;
+        }
+
+        if (Craft::$app->getPlugins()->getPlugin('sprout-reports-categories')) {
+            $sources['sproutreports.categories'] = Categories::class;
+        }
+
+        $query     = new Query();
+        foreach ($sources as $key => $namespace) {
+            $id = $query->select(['id'])
+                ->from(['{{%sproutreports_datasources}}'])
+                ->where(['type' => $namespace])
+                ->scalar();
+
+            $this->update('{{%sproutreports_reports}}', ['dataSourceId' => $id], ['dataSourceId' => $key]);
+        }
     }
 
     /**

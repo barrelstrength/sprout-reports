a525c4ab52c88f2bd6109b61d7b6f91fed58a407
Andre L <andrelopezd@gmail.com>
2018-04-17 19:51:35 -0400

    Adds pluginId add column migration

---
+++ src/migrations/m180417_000000_sproutreports_call_datasources_pluginid_column.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace barrelstrength\sproutreports\migrations;
+
+use craft\db\Migration;
+use barrelstrength\sproutbase\migrations\sproutreports\m180417_000000_sproutreports_datasources_pluginid_column as SproutReportsPluginId;
+
+/**
+ * m180417_000000_sproutreports_call_datasources_pluginid_column migration.
+ */
+class m180417_000000_sproutreports_call_datasources_pluginid_column extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function safeUp()
+    {
+        $migration = new SproutReportsPluginId();
+
+        ob_start();
+        $migration->safeUp();
+        ob_end_clean();
+
+        return true;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown()
+    {
+        echo "m180417_000000_sproutreports_call_datasources_pluginid_column cannot be reverted.\n";
+        return false;
+    }
+}

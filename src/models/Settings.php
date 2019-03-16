d1031da632de0811085eaafc38bebcc7be7bbdc9
Ben Parizek <ben@barrelstrengthdesign.com>
2019-03-16 10:16:58 -0700

    Adds getSharedPermissions method

--- src/models/Settings.php
+++ src/models/Settings.php
@@ -3,8 +3,10 @@
 namespace barrelstrength\sproutreports\models;
 
 use barrelstrength\sproutbase\base\SproutSettingsInterface;
+use barrelstrength\sproutbase\SproutBase;
 use craft\base\Model;
 use Craft;
+use craft\helpers\StringHelper;
 
 /**
  *
@@ -29,6 +31,24 @@ class Settings extends Model implements SproutSettingsInterface
                 'selected' => 'general',
                 'template' => 'sprout-base-reports/settings/general'
             ]
+        ];
+    }
+
+    /**
+     * Shared permissions they may be prefixed by another plugin. Before checking
+     * these permissions the plugin name will be determined from the URL and appended.
+     *
+     * @example
+     * /admin/sprout-reports/page => sproutReports-viewReports
+     * /admin/sprout-forms/page => sproutForms-viewReports
+     *
+     * @return array
+     */
+    public static function getSharedPermissions(): array
+    {
+        return [
+            'viewReports',
+            'editReports'
         ];
     }
 }

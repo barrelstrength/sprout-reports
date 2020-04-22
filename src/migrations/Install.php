cd5c6d652bc3886467d191ddc6487fe19fa92f77
Ben Parizek <ben.parizek@gmail.com>
2020-04-21 22:11:23 -0400

    Adds uninstall migration

--- src/migrations/Install.php
+++ src/migrations/Install.php
@@ -7,7 +7,10 @@
 
 namespace barrelstrength\sproutreports\migrations;
 
+use barrelstrength\sproutbase\base\SproutDependencyInterface;
+use barrelstrength\sproutbase\migrations\Install as SproutBaseInstall;
 use barrelstrength\sproutbasereports\migrations\Install as SproutBaseReportsInstall;
+use barrelstrength\sproutreports\SproutReports;
 use craft\db\Migration;
 
 class Install extends Migration
@@ -22,6 +25,33 @@ class Install extends Migration
         ob_start();
         $migration->safeUp();
         ob_end_clean();
+
+        return true;
+    }
+
+    public function safeDown(): bool
+    {
+        /** @var SproutReports $plugin */
+        $plugin = SproutReports::getInstance();
+
+        $sproutBaseReportsInUse = $plugin->dependencyInUse(SproutDependencyInterface::SPROUT_BASE_REPORTS);
+        $sproutBaseInUse = $plugin->dependencyInUse(SproutDependencyInterface::SPROUT_BASE);
+
+        if (!$sproutBaseReportsInUse) {
+            $migration = new SproutBaseReportsInstall();
+
+            ob_start();
+            $migration->safeDown();
+            ob_end_clean();
+        }
+
+        if (!$sproutBaseInUse) {
+            $migration = new SproutBaseInstall();
+
+            ob_start();
+            $migration->safeDown();
+            ob_end_clean();
+        }
 
         return true;
     }
